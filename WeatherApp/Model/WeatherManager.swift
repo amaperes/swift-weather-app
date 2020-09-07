818fc7a49590768ef8fbbd91e3f664c9155aa1ab
amp <am.perescu@gmail.com>
2020-09-07 19:47:04 +0300

    Fetch data based on city name

--- WeatherApp/Model/WeatherManager.swift
+++ WeatherApp/Model/WeatherManager.swift
@@ -9,11 +9,42 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=29ab07ae106a6961c71c53ea4064808e&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=29ab07ae106a6961c71c53ea4064808e&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].id)
+        } catch {
+            print(error)
+        }
+    }
+        
+    
+
     
 }
